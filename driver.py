4cdf76812f5c39e00e828bbd9718fd1d304c2323
owgraham <omg28@gmail.com>
2025-03-28 23:39:25 +0000

    new file extension for saved images

--- driver.py
+++ driver.py
@@ -3,7 +3,7 @@ from download_day import download_day
 start_time_str       = '2022-02-01T00:00:00Z'
 stop_time_str        = '2022-03-01T01:00:00Z'
 # plane_callsign       = "EZY158T"
-query_limit          = 5e4
+query_limit          = 1e3
 send_notification    = True
 make_plot            = True
 
