5ade5699b73680da816a63be8aced91cd79489e8
owgraham <omg28@gmail.com>
2025-04-22 21:35:13 +0100

    still need to complete the logic and generate_flightpath code but I am going to bed now:)

---
+++ ft_to_m.py
@@ -0,0 +1,9 @@
+def ft_to_m(feet):
+    """
+    Convert feet to meters.
+    Args:
+        feet (float): The value in feet.
+    Returns:
+        float: The value in meters.
+    """
+    return feet * 0.3048
--- icet.py
+++ icet.py
@@ -1,12 +1,14 @@
 from ambiance import Atmosphere
-def icet(v_i, calibration_factor, altitude_m):
+def icet(v_i,
+         altitude_m,
+         calibration_factor: float = 0):
     """
     Calculate the equivalent airspeed (EAS) from indicated airspeed (IAS) using a calibration factor and altitude.
 
     :param indicated: Indicated airspeed in knots
     :param calibration_factor: Calibration factor for the airspeed indicator
     :param altitude_m: Altitude in meters
-    :return: True airspeed (TAS) in m/s and Mach number
+    :return: True airspeed (TAS) in m/s, Mach number, speed of sound in m/s at the given altitude
     """
     # Constants
     k = 1.4 # Ratio of specific heats for air
@@ -29,4 +31,4 @@ def icet(v_i, calibration_factor, altitude_m):
     # Equivalent -> True
     v_t = v_e*(r_0/r)**0.5
     
-    return v_t, M
+    return v_t, M, a
