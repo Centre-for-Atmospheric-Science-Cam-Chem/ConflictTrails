7c67703580ee30dd7be35b3bc448f5d227621443
owgraham <omg28@gmail.com>
2025-05-08 12:54:57 +0100

    Add remove_superseded parameter to get_engine_data function for filtering engines out that have been superseded by more recent emissions models.

--- get_engine_data.py
+++ get_engine_data.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 def get_engine_data(input_directory: str = "/scratch/omg28/Data/engine_data",
-                    output_directory: str = "/scratch/omg28/Data/engine_data"):
+                    output_directory: str = "/scratch/omg28/Data/engine_data",
+                    remove_superseded: bool = False) -> pd.DataFrame:
     """
     Loads data from the saved engine data .csv files for analysis. Saves this dataframe to a .pkl file, and also returns the loaded data as a DataFrame.
 
@@ -14,6 +15,10 @@ def get_engine_data(input_directory: str = "/scratch/omg28/Data/engine_data",
     loaded_data = pd.read_csv(filepath, index_col=0)
     # Save the data to a .pkl file
     output_filepath = os.path.join(output_directory, "engine_data_icao.pkl")
+    
+    if remove_superseded:
+        # Remove superseded engines
+        loaded_data = loaded_data[~loaded_data['Data Superseded'].str.contains('Yes', na=False)]
     
     loaded_data.to_pickle(output_filepath)
     # Return the loaded data
