34252aa3aa34eec1e4723ce2f462b8b23ebd9779
owgraham <omg28@gmail.com>
2025-05-14 20:47:01 +0100

    Refactor code structure for improved readability and maintainability

--- bffm2.py
+++ bffm2.py
@@ -9,30 +9,33 @@ def bffm2(aircraft_data, altitude, mach, W_f):
     The BFFM2 model is a proprietary model developed by Boeing, and it is based on extensive data collected from commercial aircraft operations. The model uses a combination of empirical data and theoretical models to estimate fuel consumption and emissions for different flight profiles, and it takes into account various factors such as aircraft weight, altitude, speed, and engine performance.
     '''
     # get the necessary data from the aircraft_data dictionary
-    fuel_flow_icao = np.array([aircraft_data['Fuel Flow T/O (kg/sec)'],
-                            aircraft_data['Fuel Flow C/O (kg/sec)'],
-                            aircraft_data['Fuel Flow App (kg/sec)'],
-                            aircraft_data['Fuel Flow Idle (kg/sec)']]).reshape(1,-1)
-    
-    HC_ei_vec = np.array([aircraft_data['HC EI T/O (g/kg)'],
-                          aircraft_data['HC EI C/O (g/kg)'],
-                          aircraft_data['HC EI App (g/kg)'],
-                          aircraft_data['HC EI Idle (g/kg)']]).reshape(1,-1)
-    
-    CO_ei_vec = np.array([aircraft_data['CO EI T/O (g/kg)'],
-                        aircraft_data['CO EI C/O (g/kg)'],
-                        aircraft_data['CO EI App (g/kg)'],
-                        aircraft_data['CO EI Idle (g/kg)']]).reshape(1,-1)
-    
-    NOx_ei_vec = np.array([aircraft_data['NOx EI T/O (g/kg)'],
-                           aircraft_data['NOx EI C/O (g/kg)'],
-                           aircraft_data['NOx EI App (g/kg)'],
-                           aircraft_data['NOx EI Idle (g/kg)']]).reshape(1,-1)
+    fuel_flow_icao = np.array([aircraft_data.iloc[0]['Fuel Flow T/O (kg/sec)'],
+                               aircraft_data.iloc[0]['Fuel Flow C/O (kg/sec)'],
+                               aircraft_data.iloc[0]['Fuel Flow App (kg/sec)'],
+                               aircraft_data.iloc[0]['Fuel Flow Idle (kg/sec)']])
+    print(fuel_flow_icao)                       
+
+    HC_ei_vec = np.array([aircraft_data.iloc[0]['HC EI T/O (g/kg)'],
+                          aircraft_data.iloc[0]['HC EI C/O (g/kg)'],
+                          aircraft_data.iloc[0]['HC EI App (g/kg)'],
+                          aircraft_data.iloc[0]['HC EI Idle (g/kg)']])
+    print(HC_ei_vec)
+
+    CO_ei_vec = np.array([aircraft_data.iloc[0]['CO EI T/O (g/kg)'],
+                          aircraft_data.iloc[0]['CO EI C/O (g/kg)'],
+                          aircraft_data.iloc[0]['CO EI App (g/kg)'],
+                          aircraft_data.iloc[0]['CO EI Idle (g/kg)']])
+    print(CO_ei_vec)
+
+    NOx_ei_vec = np.array([aircraft_data.iloc[0]['NOx EI T/O (g/kg)'],
+                           aircraft_data.iloc[0]['NOx EI C/O (g/kg)'],
+                           aircraft_data.iloc[0]['NOx EI App (g/kg)'],
+                           aircraft_data.iloc[0]['NOx EI Idle (g/kg)']])
+    print(NOx_ei_vec)
 
     atmosphere = Atmosphere(altitude)
     P_amb = pa_to_psi(atmosphere.pressure)
-    T_amb = atmosphere.temperature
-    
+    T_amb = Atmosphere.T2t(atmosphere.temperature)
 
     # 1. Calculate the ambient pressure and temperature at the given altitude:
 
@@ -42,18 +45,12 @@ def bffm2(aircraft_data, altitude, mach, W_f):
     #                   T/O   C/O    App    Idle                
     icf_vec = np.array([1.01, 1.013, 1.020, 1.100])
     fuel_flow_icao = np.array(fuel_flow_icao * icf_vec)
-    print(icf_vec)
-    print(fuel_flow_icao)
-    
-    # transform all to log10 space:
-    fuel_flow_icao = np.log10(fuel_flow_icao)
-    HC_ei_vec = np.log10(HC_ei_vec)
-    CO_ei_vec = np.log10(CO_ei_vec)
-    NOx_ei_vec = np.log10(NOx_ei_vec)
-    fuel_flow_evaluate = np.log10(W_f)
 
     # 2. Use linear log-log fit on the emission index vs fuel flow rate
     def piecewise_fit(x, EI_vec, fuel_flow_icao_local):
+        x = np.log(x)
+        EI_vec = np.log(EI_vec)
+        fuel_flow_icao_local = np.log(fuel_flow_icao_local)
         # Line between Idle and Approach:
         m_3_2 = (EI_vec[3] - EI_vec[2]) / (fuel_flow_icao_local[3] - fuel_flow_icao_local[2])
         b_3_2 = EI_vec[3] - m_3_2 * fuel_flow_icao_local[3]
@@ -63,23 +60,23 @@ def bffm2(aircraft_data, altitude, mach, W_f):
         b_0_1 = (EI_vec[0] + EI_vec[1])/2
         
         # find line between intersection and constant
-        intersect_x = (b_0_1 - b_3_2) / m_3_2 if m_3_2 != 0 else float('inf')           
+        intersect_x = (b_0_1 - b_3_2) / m_3_2 if m_3_2 != 0 else float('inf')
         # If intersection is within [App, C/O]
         if fuel_flow_icao_local[2] <= intersect_x <= fuel_flow_icao_local[1]:
             if x >= intersect_x:
-                return b_0_1
+                return np.exp(b_0_1)
             else:
-                return m_3_2 * x + b_3_2
+                return np.exp(m_3_2 * x + b_3_2)
         else: # if the intersection is outside of app, CO
             # find line between approach and average of climbout and takeoff:
             m_2_1 = (EI_vec[2] - b_0_1) / (fuel_flow_icao_local[2] - fuel_flow_icao_local[1])
             b_2_1 = EI_vec[2] - m_2_1 * fuel_flow_icao_local[2]
             if x >= fuel_flow_icao_local[1]: # if x is greater than climbout
-                return b_0_1
+                return np.exp(b_0_1)
             elif x <= fuel_flow_icao_local[2]: # if x is less than approach
-                return m_3_2 * x + b_3_2
+                return np.exp(m_3_2 * x + b_3_2)
             else: # if x is between approach and climbout
-                return m_2_1 * x + b_2_1
+                return np.exp(m_2_1 * x + b_2_1)
     # For NOx: Fit a piecewise continuous line through all four points
     # Create a piecewise linear point-to-point interpolator in log-log space
     EI_HC_ret = piecewise_fit(W_f, HC_ei_vec, fuel_flow_icao)
@@ -98,22 +95,21 @@ def bffm2(aircraft_data, altitude, mach, W_f):
     theta_amb = (T_amb + 273.15)/288.15
     # 2b. fuel flow values are further modified by the ambient values:
     W_ff = (W_f/del_amb) * (theta_amb**3.8) * np.exp(0.2*mach**2)
-    
     # 2c. calculate the humidity correction factor, using modified EUCONTROL correction (0.37802)
     phi = 0.6 # relative humidity, assumed to be 60% for this calculation
     beta = 7.90298*(1-373.16/(T_amb + 273.16)) + 3.00571 + (5.02808) * np.log(373.16/(T_amb + 273.16)) + 1.3816*10**(-7) * (1-10**(11.344*(1-(T_amb + 273.16)/373.16))) + 8.1328 * 10**(-3) * (10**(3.49149*(1-373.16/(T_amb+273.16)))-1)
     P_v = (0.014504)*10**beta
     omega = (0.62198*phi*P_v)/(P_amb-0.37802*phi*P_v)
-    H_corr_factor = -19*(omega-0.0063)
+    H_corr_factor = -19*(omega-0.00634)
     
     # 3. Compute the EI:
     rEI_HC = piecewise_fit(W_ff, HC_ei_vec, fuel_flow_icao)
     rEI_CO = piecewise_fit(W_ff, CO_ei_vec, fuel_flow_icao)
     rEI_NOx = np.interp(W_ff, np.flip(fuel_flow_icao), np.flip(NOx_ei_vec))
-    
+
     EI_HC_corr = rEI_HC * theta_amb**3.3 / (del_amb**1.02)
     EI_CO_corr = rEI_CO * theta_amb**3.3 / (del_amb**1.02)
-    EI_NOx_corr = rEI_NOx * (del_amb**1.02 * theta_amb**3.3)**0.5 * np.exp(H_corr_factor)   
+    EI_NOx_corr = rEI_NOx * (del_amb**1.02 / theta_amb**3.3)**0.5 * np.exp(H_corr_factor)   
     
     # 4. Calculate the final EI:
-    return EI_HC_ret, EI_CO_ret, EI_NOx_ret
+    return np.array([EI_HC_corr, EI_CO_corr, EI_NOx_corr])
